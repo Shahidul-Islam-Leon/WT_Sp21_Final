ae476a099696e1fa575764d453e5d62d9049993d
Shahidul-Islam-Leon <72728147+Shahidul-Islam-Leon@users.noreply.github.com>
2021-03-28 16:42:49 +0600

    final

--- lab1/Dashboard.php
+++ lab1/Dashboard.php
@@ -1,3 +1,9 @@
+<?php
+    if(!isset($_COOKIE["username"])){
+        header("Location: Login.php");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <h1>WELCOME <?php echo $_COOKIE["username"];?> </h1>
